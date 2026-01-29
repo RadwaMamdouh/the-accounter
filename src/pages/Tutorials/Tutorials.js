01d0b27383fff941abc4366a08abd5f9e5b991e0
Radwa El-Badawy <RElbadawy@worldofss.com>
2026-01-29 18:09:41 +0200

    handle tutorials search

--- src/pages/Tutorials/Tutorials.js
+++ src/pages/Tutorials/Tutorials.js
@@ -3,46 +3,57 @@ import Page from "components/Page/Page";
 import TutorialCard from "components/TutorialCard/TutorialCard";
 import { search } from "icons/index";
 import { InputText } from "primereact/inputtext";
+import { useMemo, useState } from "react";
+
+const tutorials = [
+	{
+		id: 1,
+		image: "/images/tutorial1.jpg",
+		title: "How to Leverage Artificial Intelligence for Your Small Business",
+		postedOn: "March 7, 2023",
+	},
+	{
+		id: 2,
+		image: "/images/tutorial2.jpg",
+		title: "How to Leverage Artificial Intelligence for Your Small Business",
+		postedOn: "March 7, 2023",
+	},
+	{
+		id: 3,
+		image: "/images/tutorial3.jpg",
+		title: "How to Leverage Artificial Intelligence for Your Small Business",
+		postedOn: "March 7, 2023",
+	},
+	{
+		id: 4,
+		image: "/images/tutorial4.jpg",
+		title: "How to Leverage Artificial Intelligence for Your Small Business",
+		postedOn: "March 7, 2023",
+	},
+	{
+		id: 5,
+		image: "/images/tutorial5.jpg",
+		title: "How to Leverage Artificial Intelligence for Your Small Business",
+		postedOn: "March 7, 2023",
+	},
+	{
+		id: 6,
+		image: "/images/tutorial6.jpg",
+		title: "How to Leverage Artificial Intelligence for Your Small Business",
+		postedOn: "March 7, 2023",
+	},
+];
 
 const Tutorials = () => {
-	const tutorials = [
-		{
-			id: 1,
-			image: "/images/tutorial1.jpg",
-			title: "How to Leverage Artificial Intelligence for Your Small Business",
-			postedOn: "March 7, 2023",
-		},
-		{
-			id: 2,
-			image: "/images/tutorial2.jpg",
-			title: "How to Leverage Artificial Intelligence for Your Small Business",
-			postedOn: "March 7, 2023",
-		},
-		{
-			id: 3,
-			image: "/images/tutorial3.jpg",
-			title: "How to Leverage Artificial Intelligence for Your Small Business",
-			postedOn: "March 7, 2023",
-		},
-		{
-			id: 4,
-			image: "/images/tutorial4.jpg",
-			title: "How to Leverage Artificial Intelligence for Your Small Business",
-			postedOn: "March 7, 2023",
-		},
-		{
-			id: 5,
-			image: "/images/tutorial5.jpg",
-			title: "How to Leverage Artificial Intelligence for Your Small Business",
-			postedOn: "March 7, 2023",
-		},
-		{
-			id: 6,
-			image: "/images/tutorial6.jpg",
-			title: "How to Leverage Artificial Intelligence for Your Small Business",
-			postedOn: "March 7, 2023",
-		},
-	];
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const filteredTutorials = useMemo(() => {
+		if (!searchTerm) return tutorials;
+
+		return tutorials.filter((tutorial) =>
+			tutorial.title.toLowerCase().includes(searchTerm.toLowerCase()),
+		);
+	}, [searchTerm]);
 
 	return (
 		<Page title="Tutorials">
@@ -58,6 +69,7 @@ const Tutorials = () => {
 						name="search"
 						placeholder="Ask a question..."
 						className="bg-white block py-[10px] lg:py-4 px-[10px] lg:px-3 !ps-10 border border-border-light rounded-md w-full outline-0 focus-visible:outline-none transition-all duration-300 hover:border-primary"
+						onChange={(e) => setSearchTerm(e.target.value)}
 					/>
 				</div>
 			</FaqsTutorialsHead>
@@ -70,7 +82,7 @@ const Tutorials = () => {
 							About The Accounter
 						</h2>
 						<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-							{tutorials.map((tutorial) => (
+							{filteredTutorials.map((tutorial) => (
 								<TutorialCard key={tutorial.id} data={tutorial} />
 							))}
 						</div>
