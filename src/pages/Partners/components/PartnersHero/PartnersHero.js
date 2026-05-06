784888cec1fd073a1e606538a7fce62c20fe1695
Radwa El-Badawy <RElbadawy@worldofss.com>
2026-05-06 11:15:37 +0300

    feat: Enhance RTL support by adding leading-relaxed class to text elements in Qtys and PartnersHero components

--- src/pages/Partners/components/PartnersHero/PartnersHero.js
+++ src/pages/Partners/components/PartnersHero/PartnersHero.js
@@ -9,10 +9,10 @@ const PartnersHero = () => {
 				<h6 className="text-sm text-primary text-center uppercase mb-4 lg:mb-6">
 					{t("ourPartners")}
 				</h6>
-				<h1 className="text-xl lg:text-4xl font-bold text-white text-center mb-4 lg:mb-3 lg:w-1/2 max-w-full mx-auto">
+				<h1 className="text-xl lg:text-4xl rtl:leading-relaxed font-bold text-white text-center mb-4 lg:mb-3 lg:w-1/2 max-w-full mx-auto">
 					{t("partnersHeroHeadline")}
 				</h1>
-				<p className="text-sm text-light text-center lg:max-w-[40%] mx-auto">
+				<p className="text-sm text-light rtl:leading-relaxed text-center lg:max-w-[40%] mx-auto">
 					{t("partnersHeroSubText")}
 				</p>
 			</div>
